d552c51313978e103eeb91a7221aeab69a546570
Rand-max <57353839+Rand-max@users.noreply.github.com>
2022-10-06 11:58:11 +0800

    spell crown respawn function
    spell crown respawn function

--- pokerwizard/Assets/Scripts/GainMagicItem.cs
+++ pokerwizard/Assets/Scripts/GainMagicItem.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class GainMagicItem : MonoBehaviour
 {
-    public GameObject pb;
+    [SerializeField]
+    public ParticleSystem boom=null;
+
+    public GameObject crown_obj;
+    public float respawn_time;
     public bool alive;
+
+    private bool par_start;
+    private Vector3 ori_scale;
     // Start is called before the first frame update
     void Start()
     {
         alive=true;
+        respawn_time=3f;
+        par_start=false;
+        ori_scale=crown_obj.transform.localScale;
+        Scaleup();
     }
 
     // Update is called once per frame
@@ -17,13 +29,31 @@ public class GainMagicItem : MonoBehaviour
     {
         if (!alive)
         {
-            Instantiate(pb, this.gameObject.transform.position, Quaternion.identity);
-            Destroy(this.gameObject);
+            crown_obj.SetActive(false);
+            respawn_time-=Time.deltaTime;
+            crown_obj.transform.DOScale(0,1f);
         }
+        if(par_start){
+            boom.Play();
+            par_start=false;
+        }
+        if(respawn_time<0){
+            respawn_time=0f;
+            alive=true;
+            Scaleup();
+        }
+        if(alive){
+            crown_obj.SetActive(true);
+            respawn_time=3f;
+        }
+    }
+    void Scaleup(){
+        crown_obj.transform.DOScale(ori_scale,1f);
     }
     void OnTriggerEnter(Collider collision){
     if(collision.gameObject.tag == "Player"){
         alive=false;
+        par_start=true;
     }
  }
 }
