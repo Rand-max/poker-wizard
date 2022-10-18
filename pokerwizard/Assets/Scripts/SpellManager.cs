718d44fc05ca03fd9f90c718139d63e0f79646c7
hokomili <hokomili@gmail.com>
2022-10-19 06:36:28 +0800

    add forgotten code

--- pokerwizard/Assets/Scripts/SpellManager.cs
+++ pokerwizard/Assets/Scripts/SpellManager.cs
@@ -14,21 +14,23 @@ public class SpellManager : MonoBehaviour
         UpdateList();
     }
     void UpdateList(){
-        tempSpell.Clear();
-        string[] assetNames = AssetDatabase.FindAssets("", new[] { "Assets/Scripts/Spell" });
+        if(FetchDataInEditor){
+            tempSpell.Clear();
+            string[] assetNames = AssetDatabase.FindAssets("", new[] { "Assets/Scripts/Spell" });
 
-        foreach (string spellname in assetNames)
-        {
-            var SOpath = AssetDatabase.GUIDToAssetPath(spellname);
-            var spell = AssetDatabase.LoadAssetAtPath<SpellData>(SOpath);
-            tempSpell.Add(spell);
-        }
-        allSpell.Clear();
-        for(int i=0;i<tempSpell.Count;i++){
-            if(tempSpell[i].ID==0&&tempSpell[i].SpellName!="Flame"){
-                tempSpell[i].ID=tempSpell.Count;
+            foreach (string spellname in assetNames)
+            {
+                var SOpath = AssetDatabase.GUIDToAssetPath(spellname);
+                var spell = AssetDatabase.LoadAssetAtPath<SpellData>(SOpath);
+                tempSpell.Add(spell);
             }
+            allSpell.Clear();
+            for(int i=0;i<tempSpell.Count;i++){
+                if(tempSpell[i].ID==0&&tempSpell[i].SpellName!="Flame"){
+                    tempSpell[i].ID=tempSpell.Count;
+                }
+            }
+            allSpell=tempSpell.OrderBy(s => s.ID).ToList();
         }
-        allSpell=tempSpell.OrderBy(s => s.ID).ToList();
     }
 }
