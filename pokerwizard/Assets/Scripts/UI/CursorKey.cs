0bd83f5bc4707e9cefc7757f6dc4fc4673886476
hokomili <hokomili@gmail.com>
2022-10-31 21:03:24 +0800

    fix cursor

--- pokerwizard/Assets/Scripts/UI/CursorKey.cs
+++ pokerwizard/Assets/Scripts/UI/CursorKey.cs
@@ -8,20 +8,22 @@ using TMPro;
 public class CursorKey : MonoBehaviour
 {
     RectTransform cursor;
+    public Texture2D cursortext;
     // Start is called before the first frame update
     void Start()
     {
         cursor= GetComponent<RectTransform>();
-        Cursor.SetCursor(GetComponent<Image>().sprite)=false;
+        Cursor.SetCursor(cursortext,Vector2.zero,CursorMode.Auto);
         //Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         Vector2 cursorpos= Mouse.current.position.ReadValue();
 
         cursor.anchoredPosition = cursorpos;
-
+        */
     }
 }
