3202764fa4ba2cd15f594e74dfcf06fc0a828f6d
hokomili <hokomili@gmail.com>
2022-11-07 21:19:45 +0800

    vm

--- pokerwizard/Assets/Scripts/UI/CursorKey.cs
+++ pokerwizard/Assets/Scripts/UI/CursorKey.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.InputSystem.UI;
 using TMPro;
 
 public class CursorKey : MonoBehaviour
@@ -10,15 +11,18 @@ public class CursorKey : MonoBehaviour
     RectTransform cursor;
     public float cursoraccel=1f;
     public float Maxspeed=5f;
-    public float cursorfloattime=0.5f;
-    public Vector2 currentaccel;
+    public float cursorfloattime=0.9f;
+    public float currentaccel;
     public Vector2 currentSpeed;
+    public VirtualMouseInput vm;
+    public float innertimer;
     // Start is called before the first frame update
     void Start()
     {
         cursor= GetComponent<RectTransform>();
         //Cursor.SetCursor(cursortext,Vector2.zero,CursorMode.Auto);
         Cursor.visible = false;
+        vm=gameObject.GetComponent<VirtualMouseInput>();
     }
 
     // Update is called once per frame
@@ -27,36 +31,31 @@ public class CursorKey : MonoBehaviour
         
         //Vector2 cursorpos= Mouse.current.position.ReadValue();
         //cursor.anchoredPosition = cursorpos;
-        if(currentSpeed.magnitude>Maxspeed){
-            currentSpeed=currentSpeed.normalized*Maxspeed;
+        vm.cursorSpeed+=currentaccel;
+        if(vm.cursorSpeed>Maxspeed){
+            vm.cursorSpeed=Maxspeed;
         }
-        currentSpeed+=currentaccel;
-        if(cursor.anchoredPosition.x>Screen.width){
-            if(currentSpeed.x>0){
-                currentSpeed.x=0;
-            }
+        
+        if(vm.cursorTransform.anchoredPosition.x>Screen.width){
+            vm.cursorTransform.anchoredPosition+=new Vector2(Screen.width-vm.cursorTransform.anchoredPosition.x,0);
         }
-        if(cursor.anchoredPosition.x<0){
-            if(currentSpeed.x<0){
-                currentSpeed.x=0;
-            }
+        if(vm.cursorTransform.anchoredPosition.x<0){
+            vm.cursorTransform.anchoredPosition+=new Vector2(-vm.cursorTransform.anchoredPosition.x,0);
         }
-        if(cursor.anchoredPosition.y>Screen.height){
-            if(currentSpeed.y>0){
-                currentSpeed.y=0;
-            }
+        if(vm.cursorTransform.anchoredPosition.y>Screen.height){
+            vm.cursorTransform.anchoredPosition+=new Vector2(0,Screen.height-vm.cursorTransform.anchoredPosition.y);
         }
-        if(cursor.anchoredPosition.y<0){
-            if(currentSpeed.y<0){
-                currentSpeed.y=0;
-            }
+        if(vm.cursorTransform.anchoredPosition.y<0){
+            vm.cursorTransform.anchoredPosition+=new Vector2(0,-vm.cursorTransform.anchoredPosition.y);
         }
-        cursor.anchoredPosition+=currentSpeed*Time.deltaTime;
-        if(currentaccel.magnitude<=new Vector2(0.1f,0.1f).magnitude){
-            currentSpeed=Vector2.Lerp(currentSpeed,Vector2.zero,cursorfloattime);
+        //vm.cursorTransform.anchoredPosition+=currentSpeed*Time.deltaTime;
+        //cursor.anchoredPosition+=currentSpeed*Time.deltaTime;
+        if(currentaccel<=new Vector2(0.1f,0.1f).magnitude){
+            //currentSpeed=Vector2.Lerp(currentSpeed,Vector2.zero,cursorfloattime);
+            vm.cursorSpeed=cursorfloattime/vm.cursorSpeed;
         }
     }
     public void movecursor(Vector2 dire){
-        currentaccel=dire*cursoraccel;
+        currentaccel=dire.magnitude*cursoraccel;
     }
 }
