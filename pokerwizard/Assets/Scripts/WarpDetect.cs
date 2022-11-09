deffa5d482576d4dc065a03c77c4d508414c122e
Rand-max <57353839+Rand-max@users.noreply.github.com>
2022-11-09 22:28:55 +0800

    upgrade warp

--- pokerwizard/Assets/Scripts/WarpDetect.cs
+++ pokerwizard/Assets/Scripts/WarpDetect.cs
@@ -6,13 +6,16 @@ using UnityEngine.VFX;
 public class WarpDetect : MonoBehaviour
 {
     public VisualEffect warpSpeedVfx;
+    public MeshRenderer tunnelLight;
     public bool warpActive;
     public float rate=0.02f;
+    public float delay=3f;
     // Start is called before the first frame update
     void Start()
     {
         warpSpeedVfx.Stop();
         warpSpeedVfx.SetFloat("WarpAmount",0);
+        tunnelLight.material.SetFloat("_Active",0);
     }
 
     // Update is called once per frame
@@ -21,12 +24,15 @@ public class WarpDetect : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space)){
             warpActive=true;
             StartCoroutine(ActivateParticles());
+            StartCoroutine(ActivateShader());
         }
         if(Input.GetKeyUp(KeyCode.Space)){
             warpActive=false;
             StartCoroutine(ActivateParticles());
+            StartCoroutine(ActivateShader());
         }
     }
+    //warp par
     IEnumerator ActivateParticles(){
         if(warpActive){
             warpSpeedVfx.Play();
@@ -37,7 +43,6 @@ public class WarpDetect : MonoBehaviour
                 yield return new WaitForSeconds(0.1f);
             }
         }else{
-            warpSpeedVfx.Stop();
             float amount=warpSpeedVfx.GetFloat("WarpAmount");
             while(amount>0 & !warpActive){
                 amount=-rate;
@@ -47,6 +52,29 @@ public class WarpDetect : MonoBehaviour
                     amount=0;
                     warpSpeedVfx.SetFloat("WarpAmount",amount);
                     warpSpeedVfx.Stop();
+                }
+            }
+        }
+    }
+    //tunnel
+    IEnumerator ActivateShader(){
+        if(warpActive){
+            yield return new WaitForSeconds (delay);
+            float amount=tunnelLight.material.GetFloat("_Active");
+            while(amount<1 & warpActive){
+                amount=+rate;
+                tunnelLight.material.SetFloat("_Active",amount);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }else{
+            float amount=tunnelLight.material.GetFloat("_Active");
+            while(amount>0 & !warpActive){
+                amount=-rate;
+                warpSpeedVfx.SetFloat("WarpAmount",amount);
+                yield return new WaitForSeconds(0.1f);
+                if(amount<=0+rate){
+                    amount=0;
+                    tunnelLight.material.SetFloat("_Active",amount);
                 }
             }
         }
