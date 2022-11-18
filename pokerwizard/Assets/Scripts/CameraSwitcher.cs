9f99a92b8367e146bc30753fba742e07c16984a3
Rand-max <57353839+Rand-max@users.noreply.github.com>
2022-11-18 16:10:40 +0800

    add aimpoint,music source,cinemachine track

--- pokerwizard/Assets/Scripts/CameraSwitcher.cs
+++ pokerwizard/Assets/Scripts/CameraSwitcher.cs
@@ -5,31 +5,51 @@ using UnityEngine;
 public class CameraSwitcher : MonoBehaviour
 {
     public GameObject[] cameraList;
+    public float countdown;
+    public  bool isCounting;
     private int currentCamera;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < cameraList.Length; i++){
-            cameraList[i].gameObject.SetActive(false);
-        }
+        // for (int i = 0; i < cameraList.Length; i++){
+        //     cameraList[i].gameObject.SetActive(false);
+        // }
      
-        if (cameraList.Length > 0){
-            cameraList[0].gameObject.SetActive (true);
-        }
+        // if (cameraList.Length > 0){
+        //     cameraList[0].gameObject.SetActive (true);
+        // }
+        countdown=3;
+        cameraList[0].gameObject.SetActive(true);
+        cameraList[1].gameObject.SetActive(false);
+        isCounting=true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonUp(0)){
-            currentCamera ++;
-            if (currentCamera < cameraList.Length){
-                cameraList[currentCamera - 1].gameObject.SetActive(false);
-                cameraList[currentCamera].gameObject.SetActive(true);
-            }
-            else {
-                cameraList[currentCamera].gameObject.SetActive(true);
-            }
+        // if (Input.GetMouseButtonUp(0)){
+        //     currentCamera ++;
+        //     if (currentCamera < cameraList.Length){
+        //         cameraList[currentCamera - 1].gameObject.SetActive(false);
+        //         cameraList[currentCamera].gameObject.SetActive(true);
+        //     }
+        //     else {
+        //         cameraList[currentCamera].gameObject.SetActive(true);
+        //     }
+        // }
+        if(isCounting){
+            countdown-=Time.deltaTime;
+        }
+        if(countdown<0){
+            countdown=0;
+        }
+        if(countdown==0){
+            cameraList[0].gameObject.SetActive(false);
+            cameraList[1].gameObject.SetActive(true);
+            isCounting=false;
+        }
+        if(!isCounting){
+            countdown=3;
         }
     }
 }
