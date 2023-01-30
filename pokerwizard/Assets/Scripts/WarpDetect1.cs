ce8f635422c160a68703b30fd074b45f1a54e8d7
Rand-max <57353839+Rand-max@users.noreply.github.com>
2023-01-30 19:19:23 +0800

    add warp detect

---
+++ pokerwizard/Assets/Scripts/WarpDetect1.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class WarpDetect1 : MonoBehaviour
+{
+    [SerializeField]
+    public VisualEffect warpEffect=null;
+    public GameObject tcyl;
+    public bool isTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTrigger=false;
+        tcyl.SetActive(false);
+        warpEffect.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isTrigger){
+            warpEffect.Play();
+            FindObjectOfType<AudioManager>().Play("fire_cat");
+            tcyl.SetActive(true);
+        }else{
+            warpEffect.Stop();
+            tcyl.SetActive(false);
+        }
+    }
+    //play par while in trigger,stop while out trigger
+    void OnTriggerStay(Collider collision) {
+        if(collision.gameObject.GetComponent<PlayerController>()!=null){
+            isTrigger=true;
+        }
+    }
+    void OnTriggerExit(Collider collision) {
+        isTrigger=false;
+    }
+}
