bca0336d6534c6d52b0bcb02627c3d31d0d4b3fb
Rand-max <57353839+Rand-max@users.noreply.github.com>
2023-03-06 22:19:53 +0800

    animation to ui

--- pokerwizard/Assets/Scripts/UI/MapCallOut.cs
+++ pokerwizard/Assets/Scripts/UI/MapCallOut.cs
@@ -19,11 +19,14 @@ public class MapCallOut : MonoBehaviour
     public RawImage mapGate;
     public Texture[] gateTex;
     public Image whiteGate;
+    public float gate_cd=1f;
+    public bool gatecdAllowed;
     //minimap
     public RawImage miniMap;
     public Texture[] miniTex;
     //phone
-    public GameObject phoneNum;
+    public Animator phoneAni;
+
     public TextMeshProUGUI title;
     public TextMeshProUGUI uiTag;
     public float fadeTime=1f;
@@ -38,16 +41,14 @@ public class MapCallOut : MonoBehaviour
         FadeInable=false;
         startCtn=false;
         switchAble=false;
+        gatecdAllowed=false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //wizard fade out,map fade in
-        if(Keyboard.current.zKey.wasPressedThisFrame){
-            WizardFadeOut();
-            startCtn=true;
-        }
+        StartMapUI();
         if(startCtn){
             countDown-=Time.deltaTime;
             if(countDown<0f){
@@ -65,8 +66,16 @@ public class MapCallOut : MonoBehaviour
         //switch page
         if(switchAble){
             SwitchGate();
+            ChangeGateImg();
             uiTag.text="Select Your Gate";
             JudgeMap();
+        }
+    }
+    //啟動
+    public void StartMapUI(){
+        if(Keyboard.current.zKey.wasPressedThisFrame){
+            WizardFadeOut();
+            startCtn=true;
         }
     }
     public void WizardFadeOut(){
@@ -78,32 +87,52 @@ public class MapCallOut : MonoBehaviour
         mapUI.transform.localPosition=new Vector3(0f,-1000f,0f);
         mapUI.DOAnchorPos(new Vector2(0f,0f),fadeTime,false).SetEase(Ease.OutCirc);
     }
-    //flip coin,play tele ani,change gate,change map
+    //flip coin,play tele ani,change gate,map,title
     public void JudgeMap(){
         if(mapNum==1){
             mapIcon.texture=mapTex[0];
             title.text="Colossal Chessboard";
             miniMap.texture=miniTex[0];
             mapGate.texture=gateTex[0];
-
+            phoneAni.SetBool("gate1",true);
+            phoneAni.SetBool("gate2",false);
         }
         if(mapNum==2){
             mapIcon.texture=mapTex[1];
             title.text="Ice Kingdom";
             miniMap.texture=miniTex[1];
             mapGate.texture=gateTex[1];
+            phoneAni.SetBool("gate1",false);
+            phoneAni.SetBool("gate2",true);
         }
     }
     public void SwitchGate(){
         if(Keyboard.current.rightArrowKey.wasPressedThisFrame){
             if(mapNum<2){
                 mapNum+=1;
+                gatecdAllowed=true;
+                gate_cd=1;
             }
         }
         if(Keyboard.current.leftArrowKey.wasPressedThisFrame){
             if(mapNum>1){
                 mapNum-=1;
+                gatecdAllowed=true;
+                gate_cd=1;
             }
         }
+    }
+    //等待1秒更換
+    public void ChangeGateImg(){
+        if(gatecdAllowed){
+            gate_cd-=Time.deltaTime;
+            whiteGate.color=new Color(1.0f, 1.0f, 1.0f, 1f);
+            if(gate_cd<0){
+                gate_cd=0;
+                gatecdAllowed=false;
+                whiteGate.color=new Color(1.0f, 1.0f, 1.0f, 0f);
+            }
+        }
+
     }
 }
