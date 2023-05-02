b8d7d484ff8c5938a68e4a5b47b359b322de7162
hokomili <hokomili@gmail.com>
2023-05-02 21:31:45 +0800

    200

--- pokerwizard/Assets/Scripts/ScoreManager.cs
+++ pokerwizard/Assets/Scripts/ScoreManager.cs
@@ -78,7 +78,7 @@ public class ScoreManager : MonoBehaviour
         shootpointui[1].text=(ShootPoint[2]+ShootPoint[3]+gold[2]*5+gold[3]*5).ToString();
     }
     public void finish(int index){
-        if(rank[index]>220f){
+        if(rank[index]>200f){
             finished[index]=true;
             finishAni[index].SetActive(true);
             FindObjectOfType<AudioManager>().Play("finish");
