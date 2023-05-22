463a63cf91ec6a981900e6252a6fd64730e52b56
hokomili <hokomili@gmail.com>
2023-05-22 10:02:11 +0800

    fix place

--- pokerwizard/Assets/Scripts/placecontroller.cs
+++ pokerwizard/Assets/Scripts/placecontroller.cs
@@ -7,6 +7,7 @@ public class placecontroller : MonoBehaviour
 {
     public int playercount;
     public GameObject score;
+    public SpellActivate spellActivate;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,34 @@ public class placecontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetComponent<TextMeshProUGUI>().text=score.GetComponent<ScoreManager>().leaderboard[playercount].ToString();
+        int place=score.GetComponent<ScoreManager>().leaderboard[playercount];
+        switch(place){
+            case 0:
+                GetComponent<TextMeshProUGUI>().text=place.ToString();
+                spellActivate.placeEng.text="st";
+                GetComponent<TextMeshProUGUI>().colorGradient=spellActivate.fire_color;
+                spellActivate.placeEng.colorGradient=spellActivate.fire_color;
+                break;
+            case 1:
+                GetComponent<TextMeshProUGUI>().text=place.ToString();
+                spellActivate.placeEng.text="nd";
+                GetComponent<TextMeshProUGUI>().colorGradient=spellActivate.ice_color;
+                spellActivate.placeEng.colorGradient=spellActivate.ice_color;
+                break;
+            case 2:
+                GetComponent<TextMeshProUGUI>().text=place.ToString();
+                spellActivate.placeEng.text="rd";
+                GetComponent<TextMeshProUGUI>().colorGradient=spellActivate.brown_color;
+                spellActivate.placeEng.colorGradient=spellActivate.brown_color;
+                break;
+            case 3 :
+                GetComponent<TextMeshProUGUI>().text=place.ToString();
+                spellActivate.placeEng.text="th";
+                GetComponent<TextMeshProUGUI>().colorGradient=spellActivate.purp_color;
+                spellActivate.placeEng.colorGradient=spellActivate.purp_color;
+                break;
+            default:
+                break;
+        }
     }
 }
